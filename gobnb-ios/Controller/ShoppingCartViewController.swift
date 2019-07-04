51415574c8e3ecb933ec6641816c91aae7aa015e
Hammad Tariq <hammadtariq@yahoo.co.uk>
2019-07-04 18:08:16 +0500

    Shopping cart viewcontroller under design

--- gobnb-ios/Controller/ShoppingCartViewController.swift
+++ gobnb-ios/Controller/ShoppingCartViewController.swift
@@ -16,15 +16,9 @@ class ShoppingCartViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismissButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+    
 
 }
