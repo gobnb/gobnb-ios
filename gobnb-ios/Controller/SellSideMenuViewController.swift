ced0ce64fecc5ca23ccf895afe9c5def55a09a72
Hammad Tariq <hammadtariq@yahoo.co.uk>
2019-07-17 18:00:50 +0500

    Started adding the sell side viewcontrollers

---
+++ gobnb-ios/Controller/SellSideMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SellSideMenuViewController.swift
+//  gobnb-ios
+//
+//  Created by Hammad Tariq on 17/07/2019.
+//  Copyright © 2019 Hammad Tariq. All rights reserved.
+//
+
+import UIKit
+
+class SellSideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
