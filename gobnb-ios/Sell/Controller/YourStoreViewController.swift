7efef5cb61542e466fdb7233fc1e8faf51a74439
Hammad Tariq <hammadtariq@yahoo.co.uk>
2019-07-23 17:44:07 +0500

    Image uploaded

--- gobnb-ios/Sell/Controller/YourStoreViewController.swift
+++ gobnb-ios/Sell/Controller/YourStoreViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import CropViewController
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
 
 class YourStoreViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UIImagePickerControllerDelegate, CropViewControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var addEditPictureOutlet: UIButton!
@@ -97,6 +100,18 @@ class YourStoreViewController: UIViewController, UIPickerViewDataSource, UIPicke
     }
     
     @IBAction func submitButtonTapped(_ sender: Any) {
+        print("buttontapped")
+        if let imageData = pickedImage.image?.jpeg(.lowest) {
+            print(imageData.count)
+            //let data = UIImageJPEGRepresentation(pickedImage.image!, 1.0)
+            //let data = pickedImage.image?.pngData()
+            //let parameters = [String : Any]
+            if(imageData.count > 1){
+            let parameters = ["fName" : "hammad", "lName": "tariq"]
+            requestWith(url: "http://zerobillion.com/binancepay/insertStore.php", imageData: imageData, parameters: parameters)
+            }
+        }
+        
     }
     
     //PickerView functions
@@ -112,5 +127,77 @@ class YourStoreViewController: UIViewController, UIPickerViewDataSource, UIPicke
         return supportedCurrencies[row]
     }
     
-
+    //MARK:-- Upload Functions
+    
+    func requestWith(url: String, imageData: Data?, parameters: [String : Any], onCompletion: ((JSON?) -> Void)? = nil, onError: ((Error?) -> Void)? = nil){
+        
+        print("inside requestwith")
+        //let url = "http://google.com" /* your API url */
+        SVProgressHUD.show()
+        
+        let headers: HTTPHeaders = [
+            /* "Authorization": "your_access_token",  in case you need authorization header */
+            "Content-type": "multipart/form-data"
+        ]
+        
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
+            for (key, value) in parameters {
+                multipartFormData.append("\(value)".data(using: String.Encoding.utf8)!, withName: key as String)
+            }
+            
+            if let data = imageData{
+                multipartFormData.append(data, withName: "fileToUpload", fileName: "fileToUpload1223345.png", mimeType: "image/png")
+            }
+            
+        }, usingThreshold: UInt64.init(), to: url, method: .post, headers: headers) { (result) in
+            switch result{
+            case .success(let upload, _, _):
+                upload.responseJSON { response in
+                    print("Succesfully uploaded")
+                    print(response)
+                    if let json = response.data {
+                        do{
+                            let data = try JSON(data: json)
+                            print(data[0])
+                            if(data[0] != "File Uploaded"){
+                                print("File could not be uploaded")
+                            }
+                        }
+                        catch{
+                            print("JSON Error")
+                        }
+                        
+                    }
+                   
+                    SVProgressHUD.dismiss()
+                    if let err = response.error{
+                        onError?(err)
+                        return
+                    }
+                    onCompletion?(nil)
+                }
+            case .failure(let error):
+                print("Error in upload: \(error.localizedDescription)")
+                onError?(error)
+                SVProgressHUD.dismiss()
+            }
+        }
+    }
+    
+}
+extension UIImage {
+    enum JPEGQuality: CGFloat {
+        case lowest  = 0
+        case low     = 0.25
+        case medium  = 0.5
+        case high    = 0.75
+        case highest = 1
+    }
+    
+    /// Returns the data for the specified image in JPEG format.
+    /// If the image object’s underlying image data has been purged, calling this function forces that data to be reloaded into memory.
+    /// - returns: A data object containing the JPEG data, or nil if there was a problem generating the data. This function may return nil if the image has no data or if the underlying CGImageRef contains data in an unsupported bitmap format.
+    func jpeg(_ jpegQuality: JPEGQuality) -> Data? {
+        return jpegData(compressionQuality: jpegQuality.rawValue)
+    }
 }
