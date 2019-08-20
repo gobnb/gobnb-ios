970a31d293ea5663bbc42ddc5fc9dcb6a26221da
Hammad Tariq <hammadtariq@yahoo.co.uk>
2019-08-20 15:55:01 +0500

    Shopping cart items are now viewable on the payments screen

--- gobnb-ios/Buy/Controller/OrderProgressAndPaymentViewController.swift
+++ gobnb-ios/Buy/Controller/OrderProgressAndPaymentViewController.swift
@@ -11,20 +11,67 @@ import CountdownLabel
 import SVProgressHUD
 import BinanceChain
 import SwiftKeychainWrapper
+import Alamofire
+import SwiftyJSON
 
-class OrderProgressAndPaymentViewController : UIViewController {
+class PaymentItemTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var itemQty: UILabel!
+    @IBOutlet weak var itemLabel: UILabel!
+    @IBOutlet weak var itemPrice: UILabel!
+}
+
+class OrderProgressAndPaymentViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var countdown: CountdownLabel!
     @IBOutlet weak var shoppingCartView: ShoppingCartView!
     var totalPriceInCart : Double = 0.00
     var totalItemsInCart : Int = 0
     var addressToPay:String = ""
+    var ordersArray = [[String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65.0
         countdown.setCountDownTime(minutes: 60*30)
         countdown.start()
+        let uuid = Constants.basicUUID.sha256()
+        let walletAddress = KeychainWrapper.standard.string(forKey: "walletAddress")!
+        let addressToQuery = "\(Constants.backendServerURLBase)getOrders.php?address=\(walletAddress)&uuid=\(uuid)&buy_or_sell=buy"
+        fetchOrders(url: addressToQuery)
         }
+    
+    func fetchOrders(url: String){
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    let resultJSON : JSON = JSON(response.result.value!)
+                    if(resultJSON[0] != "No orders"){
+                        for result in resultJSON{
+                            var indiResult = [String]()
+                            indiResult.append(result.1["order_id"].string ?? "");
+                            indiResult.append(result.1["order_total"].string ?? "");
+                            indiResult.append(result.1["order_currency"].string ?? "");
+                            indiResult.append(result.1["payment_done"].string ?? "");
+                            indiResult.append(result.1["order_time"].string ?? "");
+                            self.ordersArray.append(indiResult);
+                            SVProgressHUD.dismiss()
+                            self.tableView.reloadData()
+                        }
+                    }else{
+                        SVProgressHUD.dismiss()
+                    }
+                    
+                }else{
+                    SVProgressHUD.dismiss()
+                }
+        }
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         totalPriceInCart = UserDefaults.standard.double(forKey: "totalPriceInCart")
@@ -34,6 +81,36 @@ class OrderProgressAndPaymentViewController : UIViewController {
         shoppingCartView.viewCartButton.setTitle("Pay Now", for: .normal)
         shoppingCartView.viewCartButton.addTarget(self, action: Selector(("paymentButtonTapped:")), for: .touchUpInside)
         addressToPay = UserDefaults.standard.string(forKey: "peopleAddress") ?? ""
+    }
+    
+    //MARK:- TableView Functions
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ShoppingCartModel.shoppingCartArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "paymentItemsCell", for: indexPath) as! PaymentItemTableViewCell
+        //var order = ordersArray[indexPath.item]
+        cell.itemLabel?.text = ShoppingCartModel.shoppingCartArray[indexPath.row].name
+        cell.itemQty.text = "\(ShoppingCartModel.shoppingCartArray[indexPath.row].qty)"
+        let updatedPriceAfterQty = "\(Double(ShoppingCartModel.shoppingCartArray[indexPath.row].qty) * ShoppingCartModel.shoppingCartArray[indexPath.row].price)"
+        cell.itemPrice.text = "\(updatedPriceAfterQty) \(UserDefaults.standard.string(forKey: "storeBaseCurrency") ?? "")"
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        if (indexPath.row % 2 == 0){
+            cell.backgroundColor = #colorLiteral(red: 0, green: 0.7215686275, blue: 0.5803921569, alpha: 1)
+            cell.itemLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            cell.itemPrice.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            cell.itemQty.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }else{
+            cell.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        return cell
+    }
+    
+    // UITableViewAutomaticDimension calculates height of label contents/text
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
     
     @IBAction func closeButtonPressed(_ sender: Any) {
@@ -48,7 +125,7 @@ class OrderProgressAndPaymentViewController : UIViewController {
             let wallet = Wallet(mnemonic: walletKey!, endpoint: .testnet)
             wallet.synchronise() { (error) in
                 
-                print("wallet.init", wallet, error)
+                //print("wallet.init", wallet, error)
                 let currencySymbol = UserDefaults.standard.string(forKey: "storeBaseCurrency") ?? ""
                 // Create a new transfer
                 let msgTransfer = Message.transfer(symbol: currencySymbol, amount: self.totalPriceInCart, to: self.addressToPay, wallet: wallet)
